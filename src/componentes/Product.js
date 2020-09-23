587c3de06cf76425164c9417ed2466ea5d485af3
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-09-23 16:49:46 +0200

    added products and props to products

--- src/componentes/Product.js
+++ src/componentes/Product.js
@@ -1,26 +1,24 @@
 import React from "react";
 import "./Product.css";
 
-function Product() {
+function Product({ title, image, price, rating }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The lean startup</p>
+        <p>{title}</p>
         <p className="product__price">
-          <strong>20,99</strong>
+          <strong>{price}</strong>
           <small>€</small>
         </p>
         <div className="product__rating">
-          <p>🌟</p>
-          <p>🌟</p>
-          <p>🌟</p>
-          <p>🌟</p>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>🌟</p>
+            ))}
         </div>
       </div>
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
-        alt="product"
-      />
+      <img src={image} alt="product" />
       <button>Add to Basket</button>
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,16 +12,64 @@ function Home() {
           alt="banner"
         ></img>
         <div className="home__row">
-          <Product />
-          <Product />
+          <Product
+            title={
+              "The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses"
+            }
+            price={9.48}
+            image={
+              "https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+            }
+            rating={5}
+          />
+          <Product
+            title={"Apple iPhone 11 (128 Gb) - Violette"}
+            price={754.3}
+            image={
+              "https://m.media-amazon.com/images/I/71xn9bCRfhL._AC_UY436_FMwebp_QL65_.jpg"
+            }
+            rating={4}
+          />
         </div>
         <div className="home__row">
-          <Product />
-          <Product />
-          <Product />
+          <Product
+            title={
+              "Elon Musk: Wie Elon Musk die Welt verändert – Die Biografie"
+            }
+            price={19.99}
+            image={
+              "https://m.media-amazon.com/images/I/81Dsfho-uML._AC_UY436_FMwebp_QL65_.jpg"
+            }
+            rating={5}
+          />
+          <Product
+            title={
+              'neues Apple MacBook Pro (16", 16GB RAM, 512GB Speicherplatz) - Space Grau'
+            }
+            price={2719.0}
+            image={
+              "https://m.media-amazon.com/images/I/71L2iBSyyOL._AC_UY436_FMwebp_QL65_.jpg"
+            }
+            rating={5}
+          />
+          <Product
+            title={"Senor Developer | Entwickler Programmierer Nerd Shirt"}
+            price={13.49}
+            image={
+              "https://m.media-amazon.com/images/I/71q1JXoXTBL._AC_UY436_FMwebp_QL65_.jpg"
+            }
+            rating={4}
+          />
         </div>
         <div className="home__row">
-          <Product />
+          <Product
+            title={"Samsung C49HG90DMU LED Monitor (49 Zoll)"}
+            price={739.0}
+            image={
+              "https://m.media-amazon.com/images/I/81vlA84pg6L._AC_UY436_FMwebp_QL65_.jpg"
+            }
+            rating={4}
+          />
         </div>
       </div>
     </div>
