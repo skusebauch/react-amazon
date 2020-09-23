25e0190183a1eda1a058a2807860d306daf7d98a
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-09-23 11:36:18 +0200

    set up starter

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyByhvwbUiepzrX302E4xIl7DmI_6UlzUOs",
+  authDomain: "react-5453f.firebaseapp.com",
+  databaseURL: "https://react-5453f.firebaseio.com",
+  projectId: "react-5453f",
+  storageBucket: "react-5453f.appspot.com",
+  messagingSenderId: "960226960815",
+  appId: "1:960226960815:web:57eae1f1d8b34b1bc1fb5a",
+};
