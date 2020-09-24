1f563d5a324cb2b9c1338802a6c9fbea0f0d395c
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-09-24 12:39:30 +0200

    add remove from basket action

--- src/componentes/CheckoutProduct.js
+++ src/componentes/CheckoutProduct.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { useStateValue } from "../context/StateProvider";
+import * as actionType from "../context/reducer/actionTypes";
 import "./CheckoutProduct.css";
 
 function CheckoutProduct({ id, image, title, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const removeFromBasket = () => {
+    dispatch({
+      type: actionType.REMOVE_FROM_BASKET,
+      id: id,
+    });
+  };
+
   return (
     <div className="checkoutProduct">
       <img className="checkoutProduct__image" src={image} alt="product" />
@@ -18,7 +29,7 @@ function CheckoutProduct({ id, image, title, price, rating }) {
               <p>🌟</p>
             ))}
         </div>
-        <button>Remove from basket</button>
+        <button onClick={removeFromBasket}>Remove from basket</button>
       </div>
     </div>
   );
