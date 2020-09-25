a132458e90103aabbc64d7af6e23be920d82f436
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-09-25 12:08:33 +0200

    init firebase for email password auth

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { auth } from "../Keys/firebase";
+import { db } from "../Keys/firebase";
 import "./Login.css";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (event) => {
+    event.preventDefault();
+    // firebase login func
+  };
+
+  const register = (event) => {
+    event.preventDefault();
+    // firebase register func
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        // successfully created new user
+        console.log(auth);
+      })
+      .catch((error) => alert(error.message));
+  };
+
   return (
     <div className="login">
       <Link to="/">
@@ -16,16 +38,30 @@ function Login() {
         <h1>Sign-in</h1>
         <form>
           <h5>E-Mail</h5>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
           <h5>Password</h5>
-          <input type="password" />
-          <button className="login__signInButton">Sign-in</button>
+          <input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <button
+            className="login__signInButton"
+            type="submit"
+            onClick={signIn}
+          >
+            Sign-in
+          </button>
           <p>
             By continuing, you agree to Sven's FAKE Amazon's Conditions of Use
             and Privacy Notice.
           </p>
           <p className="login__newText">New to Amazon?</p>
-          <button className="login__registerButton">
+          <button className="login__registerButton" onClick={register}>
             Create your Amazon account
           </button>
         </form>
