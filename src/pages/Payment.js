09361fb3f93606932abe02572fc0feafc7f70cef
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-09-26 12:19:57 +0200

    add payment page component

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,9 +1,47 @@
 import React from "react";
+import CheckoutProduct from "../componentes/CheckoutProduct";
+import { useStateValue } from "../context/StateProvider";
+import "./Payment.css";
 
 function Payment() {
+  const [{ basket, user }, dispatch] = useStateValue();
+
   return (
     <div className="payment">
-      <h1>I am the payment site</h1>
+      <div className="payment__container">
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Delivery Address</h3>
+          </div>
+          <div className="payment__address">
+            <p>{user?.email}</p>
+            <p>Altvaterstraße 25</p>
+            <p>61231 Bad Nauheim</p>
+          </div>
+        </div>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review items and delivery</h3>
+          </div>
+          <div className="payment__items">
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details">{/* stripe action */}</div>
+        </div>
+      </div>
     </div>
   );
 }
