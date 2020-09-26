01cb02a05f6037f7890fdc4eb464a1a84a49e895
Sven Kusebauch <sven.kusebauch@gmail.com>
2020-09-26 14:26:01 +0200

    setup api endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,2 +1,22 @@
+const key = require("./key");
 const functions = require("firebase-functions");
 const express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")(key.stripeSecretKey);
+
+// SETUP API
+
+// - App config
+const app = express();
+// - Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+// - API routes
+app.get("/", (request, response) => {
+  response.status(200).send("hello world");
+});
+// - Listen command
+exports.api = functions.https.onRequest(app);
+
+// Example Endpoint
+// http://localhost:5001/react-5453f/us-central1/api
